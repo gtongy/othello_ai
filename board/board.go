440e190c715da0c003e72b50b1c88dde409a5b33
gtongy <32930281+gtongy@users.noreply.github.com>
2018-06-11 21:56:10 +0900

    name fix

--- board/board.go
+++ board/board.go
@@ -16,23 +16,24 @@ type Row struct {
 }
 
 type Board struct {
-	Turn bool // true: my_turn, false: your_turn
+	Turn bool
 	rows []Row
 }
 
+// board constant
 const (
-	BOARD_SIZE_X = 8
-	BOARD_SIZE_Y = 8
-	SPACE_VAL    = 0
-	WHITE_VAL    = 1
-	BLACK_VAL    = 2
+	BoardSizeX = 8
+	BoardSizeY = 8
+	SpaceVal   = 0
+	WhiteVal   = 1
+	BlackVal   = 2
 )
 
 func (b *Board) Initial() {
 	b.Turn = true
-	for i := 0; i < BOARD_SIZE_X; i++ {
+	for i := 0; i < BoardSizeX; i++ {
 		var row Row
-		for j := 0; j < BOARD_SIZE_Y; j++ {
+		for j := 0; j < BoardSizeY; j++ {
 			cell := Cell{x: i, y: j, val: initVal(i, j)}
 			row.num = i
 			row.cells = append(row.cells, cell)
@@ -41,24 +42,24 @@ func (b *Board) Initial() {
 	}
 }
 
-func (b *Board) Reverse(x, y, my_val, your_val int) bool {
-	reverceCells := b.ReverceCells(x, y, my_val, your_val)
+func (b *Board) Reverse(x, y, myVal, yourVal int) bool {
+	reverceCells := b.ReverceCells(x, y, myVal, yourVal)
 	if reverceCells != nil {
-		cellReverce(reverceCells, b, my_val)
+		cellReverce(reverceCells, b, myVal)
 		return true
 	}
 	return false
 }
 
-func (b *Board) ReverceCells(x, y, my_val, your_val int) []Cell {
-	if b.rows[x].cells[y].val != SPACE_VAL {
+func (b *Board) ReverceCells(x, y, myVal, yourVal int) []Cell {
+	if b.rows[x].cells[y].val != SpaceVal {
 		return nil
 	}
 	var children Children
 	children.set(b, x, y)
 	var reverceCells []Cell
 	for _, cell := range children.cells {
-		if cell.val != your_val {
+		if cell.val != yourVal {
 			continue
 		}
 		var targetCells []Cell
@@ -71,7 +72,7 @@ func (b *Board) ReverceCells(x, y, my_val, your_val int) []Cell {
 		exists = false
 		for xTarget >= 0 && xTarget <= 7 && yTarget >= 0 && yTarget <= 7 {
 			targetCells = append(targetCells, b.rows[xTarget].cells[yTarget])
-			if b.rows[xTarget].cells[yTarget].val == my_val {
+			if b.rows[xTarget].cells[yTarget].val == myVal {
 				exists = true
 				break
 			}
@@ -89,12 +90,12 @@ func (b *Board) ReverceCells(x, y, my_val, your_val int) []Cell {
 
 func initVal(x, y int) int {
 	if x == 3 && y == 3 || x == 4 && y == 4 {
-		return WHITE_VAL
+		return WhiteVal
 	}
 	if x == 3 && y == 4 || x == 4 && y == 3 {
-		return BLACK_VAL
+		return BlackVal
 	}
-	return SPACE_VAL
+	return SpaceVal
 }
 
 func cellReverce(cells []Cell, b *Board, color int) {
@@ -106,13 +107,13 @@ func cellReverce(cells []Cell, b *Board, color int) {
 func (b *Board) Print() {
 	for _, row := range b.rows {
 		for key, cell := range row.cells {
-			if cell.val == SPACE_VAL {
+			if cell.val == SpaceVal {
 				fmt.Print(" - ")
 			}
-			if cell.val == WHITE_VAL {
+			if cell.val == WhiteVal {
 				fmt.Print(" ● ")
 			}
-			if cell.val == BLACK_VAL {
+			if cell.val == BlackVal {
 				fmt.Print(" ○ ")
 			}
 			if key == len(row.cells)-1 {
@@ -125,7 +126,7 @@ func (b *Board) Print() {
 func (b *Board) EndGame() bool {
 	for _, row := range b.rows {
 		for _, cell := range row.cells {
-			if cell.val == SPACE_VAL {
+			if cell.val == SpaceVal {
 				return false
 			}
 		}
@@ -134,18 +135,18 @@ func (b *Board) EndGame() bool {
 }
 
 func (b *Board) HasToPut(turn bool) bool {
-	var my_val int
-	var your_val int
+	var myVal int
+	var yourVal int
 	if turn {
-		my_val = WHITE_VAL
-		your_val = BLACK_VAL
+		myVal = WhiteVal
+		yourVal = BlackVal
 	} else {
-		my_val = BLACK_VAL
-		your_val = WHITE_VAL
+		myVal = BlackVal
+		yourVal = WhiteVal
 	}
 	for _, row := range b.rows {
 		for _, cell := range row.cells {
-			if b.ReverceCells(cell.x, cell.y, my_val, your_val) != nil {
+			if b.ReverceCells(cell.x, cell.y, myVal, yourVal) != nil {
 				return true
 			}
 		}
--- main.go
+++ main.go
@@ -22,13 +22,13 @@ func main() {
 		}
 		if b.Turn == true {
 			fmt.Println("my turn")
-			if !move(b, board.WHITE_VAL, board.BLACK_VAL) {
+			if !move(b, board.WhiteVal, board.BlackVal) {
 				continue
 			}
 		}
 		if b.Turn == false {
 			fmt.Println("your turn")
-			if !move(b, board.BLACK_VAL, board.WHITE_VAL) {
+			if !move(b, board.BlackVal, board.WhiteVal) {
 				continue
 			}
 		}
@@ -38,14 +38,14 @@ func main() {
 	b.Print()
 }
 
-func move(b board.Board, my_val, your_val int) bool {
+func move(b board.Board, myVal, yourVal int) bool {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("please input x,y : ex) 1,3 |>")
 	text, _ := reader.ReadString('\n')
 	splited := strings.Split(strings.TrimRight(text, "\n"), ",")
 	x, _ := strconv.Atoi(splited[0])
 	y, _ := strconv.Atoi(splited[1])
-	if !b.Reverse(x, y, my_val, your_val) {
+	if !b.Reverse(x, y, myVal, yourVal) {
 		fmt.Println("invalid refers to the board")
 		return false
 	}
